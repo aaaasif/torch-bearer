7ac41e13fb57c79e3aea3eb57c0ee541142ed354
aaaasif <alexasif4gmail.com>
2022-02-11 10:41:16 +0600

    assets added

---
+++ src/Components/Home/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Banner from '../Banner/Banner';
+import Footer from '../Footer/Footer';
+import Header from '../Header/Header';
+
+const Home = () => {
+    return (
+        <div>
+            <Header/>
+            <Banner/>
+            <h1>This is home</h1>
+            <Footer/>
+        </div>
+    );
+};
+
+export default Home;
