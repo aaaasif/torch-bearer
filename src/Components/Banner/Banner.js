e79e121a0bfaa5599444f112a3b39478d4254243
aaaasif <alexasif4gmail.com>
2022-02-11 15:13:54 +0600

    banner added

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -1,9 +1,11 @@
 import React from 'react';
+// import { Carousel } from 'react-bootstrap';
+// import image from '../../assets/Banner.png'
 
 const Banner = () => {
     return (
         <div>
-            <h1>This is banner</h1>
+            <h1>This is header</h1>
         </div>
     );
 };
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -2,13 +2,24 @@ import React from 'react';
 import Banner from '../Banner/Banner';
 import Footer from '../Footer/Footer';
 import Header from '../Header/Header';
+import Section from '../Section/Section';
 
 const Home = () => {
     return (
         <div>
-            <Header/>
             <Banner/>
-            <h1>This is home</h1>
+            <div className='d-flex justify-content-start'>
+                <h3 className='d-flex justify-content-start'>SERVICE</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo.</p>
+            <div className=''>
+                <button>CONSULTING </button>
+                <button>PODIATRY </button>
+                <button>NUTRITION </button>
+                <button>MARKETING </button>
+                <button>PHYSIOTHERAPY </button>
+            </div>     
+            </div>
+            <Section/>
             <Footer/>
         </div>
     );
